48deba26f57b3d2c0b7e239bd25c173a492fe7e2
vipuldev <vipul.xtr@gmail.com>
2022-06-28 07:35:53 +0530

    agm-map setup

--- agent-client/src/app/modules/usermap/main/main.component.ts
+++ agent-client/src/app/modules/usermap/main/main.component.ts
@@ -1,23 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import { LocationService } from 'src/app/services/location.service';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
+import { pipe } from 'rxjs';
+import { LOCATION, LocationService } from 'src/app/services/location.service';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  styleUrls: ['./main.component.scss'],
 })
-export class MainComponent implements OnInit {
- public lat :  number = 51.678418 ;
- public lng : number =  7.809007 ;
-  constructor(private locationService : LocationService) { }
-  public iconUrl = "http://maps.google.com/mapfiles/ms/icons/cycling.png";
+export class MainComponent implements OnInit, OnDestroy, AfterViewInit {
+  public coords: [LOCATION] = [{ lat: 0, lng: 0 }];
+  constructor(private locationService: LocationService) {}
+  public iconUrl = 'http://maps.google.com/mapfiles/ms/icons/cycling.png';
 
   ngOnInit(): void {
-    this.locationService.getPosition().then(pos=>
-      {
-         this.lat= pos.lat
-         this.lng = pos.lng
-      });
-    
+    this.locationService.startLocationTracking();
+    this.locationService.coordinates.subscribe((val: LOCATION) => {
+      if (val.lat !== this.coords[this.coords.length - 1].lat) {
+        this.coords.push(val);
+      }
+    });
+  }
+  ngAfterViewInit(): void {}
+  ngOnDestroy(): void {
+    this.locationService.stopLocationTracking();
   }
 }
--- agent-client/src/app/services/location.service.ts
+++ agent-client/src/app/services/location.service.ts
@@ -1,23 +1,42 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
+import { Subscription, interval } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LocationService {
+  coordinates: EventEmitter<LOCATION> = new EventEmitter<LOCATION>();
+  public data: LOCATION = { lat: 0, lng: 0 };
+  private intervalSubscriber: Subscription = new Subscription();
+  constructor() {}
+  getPosition(): void {
+    navigator.geolocation.getCurrentPosition(
+      (resp) => {
+        this.data = {
+          lat: resp.coords.latitude,
+          lng: resp.coords.longitude,
+        };
 
-  constructor() { }
-  getPosition(): Promise<any>
-  {
-    return new Promise((resolve, reject) => {
+        this.coordinates.emit(this.data);
+      },
 
-      navigator.geolocation.getCurrentPosition(resp => {
-
-          resolve({lng: resp.coords.longitude, lat: resp.coords.latitude});
-        },
-        err => {
-          reject(err);
-        });
-    });
-
+      (err) => {
+        console.error(err);
+      }
+    );
   }
+  public startLocationTracking(): void {
+    this.intervalSubscriber = interval(1000).subscribe(() =>
+      this.getPosition()
+    );
+  }
+  public stopLocationTracking(): void {
+    if (this.intervalSubscriber) {
+      this.intervalSubscriber.unsubscribe();
+    }
+  }
+}
+export interface LOCATION {
+  lat: number;
+  lng: number;
 }
