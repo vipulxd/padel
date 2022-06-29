d6583af794358675f5646327c3d910808e883852
vipuldev <vipul.xtr@gmail.com>
2022-06-30 02:38:57 +0530

    Android package update , location request modifications

--- agent-client/src/app/modules/usermap/main/main.component.ts
+++ agent-client/src/app/modules/usermap/main/main.component.ts
@@ -1,56 +1,55 @@
-import {AfterViewInit, Component, OnDestroy, OnInit} from '@angular/core';
-import {LOCATION, LocationService} from 'src/app/services/location.service';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
+import { LOCATION, LocationService } from 'src/app/services/location.service';
 
 @Component({
-    selector: 'app-main',
-    templateUrl: './main.component.html',
-    styleUrls: ['./main.component.scss'],
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.scss'],
 })
 export class MainComponent implements OnInit, AfterViewInit {
-    public coords: [LOCATION] = [{lat: 0, lng: 0}];
-    public updatingLocation : boolean = false
+  public coords: [LOCATION] = [{ lat: 0, lng: 0 }];
+  public updatingLocation: boolean = false;
 
-    constructor(private locationService: LocationService) {
-    }
+  constructor(private locationService: LocationService) {}
 
-    public icon = {
-
-            scaledSize: {
-                width: 50,
-                height: 50
-            }, url: './../../assets/icons/location.svg'
-
-    }
-    ngOnInit(): void {
-        /**
-         * set loader
-         */
-        /**
-         * start continuous locations watch
-         */
-        this.locationService.coordinates.subscribe((val: LOCATION) => {
-            if (val.lat !== this.coords[this.coords.length - 1].lat) {
-                if (this.coords[0].lat == 0) {
-                    this.coords.pop()
-                }
-                this.coords.push(val);
-            }
-        });
-        /**
-         * Request for a single location update
-         */
-        this.locationService.getCurrentLocation()
-    }
-    public requestLocationUpdate(){
-        this.updatingLocation = true
-        this.locationService.startLocationTracking();
-    }
-    public requestStopLocationUpdates(){
-        this.updatingLocation = false;
-        this.locationService.stopLocationTracking();
-    }
-    ngAfterViewInit(): void {
-        this.locationService.getCurrentLocation()
-    }
-
+  public icon = {
+    scaledSize: {
+      width: 50,
+      height: 50,
+    },
+    url: './../../assets/icons/location.svg',
+  };
+  ngOnInit(): void {
+    /**
+     * set loader
+     */
+    /**
+     * start continuous locations watch
+     */
+    this.locationService.coordinates.subscribe((val: LOCATION) => {
+      if (this.coords[0].lat == 0) {
+        this.coords.pop();
+      }
+      this.coords.push(val);
+    });
+    /**
+     * Request for a single location update
+     */
+    this.locationService.getCurrentLocation();
+    /**
+     * Get status of location updates
+     */
+    this.updatingLocation = this.locationService.isUpdatingLocation;
+  }
+  public requestLocationUpdate() {
+    this.updatingLocation = true;
+    this.locationService.startLocationTracking();
+  }
+  public requestStopLocationUpdates() {
+    this.updatingLocation = false;
+    this.locationService.stopLocationTracking();
+  }
+  ngAfterViewInit(): void {
+    this.locationService.getCurrentLocation();
+  }
 }
--- agent-client/src/app/services/location.service.ts
+++ agent-client/src/app/services/location.service.ts
@@ -1,98 +1,111 @@
-import {Injectable, EventEmitter} from '@angular/core';
-import {Subscription, interval, Observable} from 'rxjs';
-import {Geolocation, Position} from '@capacitor/geolocation';
+import { Injectable, EventEmitter } from '@angular/core';
+import { Subscription, interval, Observable } from 'rxjs';
+import { Geolocation } from '@capacitor/geolocation';
 
 @Injectable({
-    providedIn: 'root',
+  providedIn: 'root',
 })
 export class LocationService {
-    public isLoading: EventEmitter<boolean> = new EventEmitter<boolean>();
-    // location callback id
-    public id: any;
-    coordinates: EventEmitter<LOCATION> = new EventEmitter<LOCATION>();
-    // current valid location fetched
-    public currentLocationInfo: LOCATION = {lat: 0, lng: 0};
-    // location captured 60 Meters before
-    public previousLocationInfo: LOCATION = {lat: 0, lng: 0};
-    // internal locations fetched from coordinates
-    public internalLocationInfo: LOCATION = {lat: 0, lng: 0};
-    private intervalSubscriber: Subscription = new Subscription();
+  public isLoading: EventEmitter<boolean> = new EventEmitter<boolean>();
+  public isUpdatingLocation: boolean = false;
+  // location callback id
+  public id: any;
+  coordinates: EventEmitter<LOCATION> = new EventEmitter<LOCATION>();
+  // current valid location fetched
+  public currentLocationInfo: LOCATION = { lat: 0, lng: 0 };
+  // location captured 60 Meters before
+  public previousLocationInfo: LOCATION = { lat: 0, lng: 0 };
+  // internal locations fetched from coordinates
+  public internalLocationInfo: LOCATION = { lat: 0, lng: 0 };
+  private intervalSubscriber: Subscription = new Subscription();
 
-    constructor() {
-    }
+  constructor() {}
 
-    async getPosition() {
-        this.id = Geolocation.watchPosition({}, (pos: Position | null, err) => {
-            if (pos) {
-                this.currentLocationInfo = {
-                    lat: pos.coords.latitude,
-                    lng: pos.coords.longitude,
-                };
+  async getPosition() {
+    await Geolocation.getCurrentPosition().then((pos) => {
+      if (pos) {
+        this.currentLocationInfo = {
+          lat: pos.coords.latitude,
+          lng: pos.coords.longitude,
+        };
+        /**
+         * If location from GPS is > 50 acc. discard
+         */
+        this.internalLocationInfo = this.currentLocationInfo;
+        if (this.previousLocationInfo != this.currentLocationInfo) {
+          this.coordinates.emit(this.currentLocationInfo);
+          var distanceFromPrevious = this.distance(
+            this.previousLocationInfo.lat,
+            this.previousLocationInfo.lng,
+            this.currentLocationInfo.lat,
+            this.currentLocationInfo.lng
+          );
 
-                this.internalLocationInfo = this.currentLocationInfo
-                if (this.previousLocationInfo != this.currentLocationInfo) {
-                    var distanceFromPrevious = this.distance(this.previousLocationInfo.lat, this.previousLocationInfo.lng, this.currentLocationInfo.lat, this.currentLocationInfo.lng)
-
-                    if (distanceFromPrevious >= 60) {
-                        this.previousLocationInfo = this.currentLocationInfo
-                        this.coordinates.emit(this.currentLocationInfo);
-                    } else {
-                        console.log(`Agent travelled ${distanceFromPrevious}M discarding`)
-                    }
-                }
+          if (distanceFromPrevious >= 60) {
+            if (pos.coords.accuracy < 40) {
+              this.previousLocationInfo = this.currentLocationInfo;
             }
-        })
-
-    }
-public getCurrentLocation(){
-this.isLoading.emit(true)
-        Geolocation.getCurrentPosition()
-            .then(pos =>{
-                this.isLoading.emit(false)
-             this.coordinates.emit({lat:pos.coords.latitude,lng:pos.coords.longitude})
-            })
-            .catch(
-                err=>{
-                    return {lat:0,lng:0}
-                }
-            )
-}
-    distance(lat1: number, lon1: number, lat2: number, lon2: number) {
-        if ((lat1 == lat2) && (lon1 == lon2)) {
-            return 0;
-        } else {
-            var radlat1 = Math.PI * lat1 / 180;
-            var radlat2 = Math.PI * lat2 / 180;
-            var theta = lon1 - lon2;
-            var radtheta = Math.PI * theta / 180;
-            var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
-            if (dist > 1) {
-                dist = 1;
-            }
-            dist = Math.acos(dist);
-            dist = dist * 180 / Math.PI;
-            dist = dist * 60 * 1.1515;
-            dist = dist * 0.8684
+          } else {
+            console.log(
+              `Agent travelled ${distanceFromPrevious}M discarding with accuracy ${pos.coords.accuracy}`
+            );
+          }
         }
-
-        return dist;
+      }
+    });
+  }
+  public getCurrentLocation() {
+    this.isLoading.emit(true);
+    Geolocation.getCurrentPosition()
+      .then((pos) => {
+        this.isLoading.emit(false);
+        this.coordinates.emit({
+          lat: pos.coords.latitude,
+          lng: pos.coords.longitude,
+        });
+      })
+      .catch((err) => {
+        return { lat: 0, lng: 0 };
+      });
+  }
+  distance(lat1: number, lon1: number, lat2: number, lon2: number) {
+    if (lat1 == lat2 && lon1 == lon2) {
+      return 0;
+    } else {
+      var radlat1 = (Math.PI * lat1) / 180;
+      var radlat2 = (Math.PI * lat2) / 180;
+      var theta = lon1 - lon2;
+      var radtheta = (Math.PI * theta) / 180;
+      var dist =
+        Math.sin(radlat1) * Math.sin(radlat2) +
+        Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+      if (dist > 1) {
+        dist = 1;
+      }
+      dist = Math.acos(dist);
+      dist = (dist * 180) / Math.PI;
+      dist = dist * 60 * 1.1515;
+      dist = dist * 0.8684;
     }
 
-    public startLocationTracking(): void {
-        // get location updates in every 10 seconds
-        this.intervalSubscriber = interval(10000).subscribe(() =>
-            this.getPosition()
-        );
-    }
+    return dist;
+  }
 
-    public stopLocationTracking(): void {
-        if (this.id) {
-            Geolocation.clearWatch({id: this.id});
-        }
-    }
+  public startLocationTracking(): void {
+    this.isUpdatingLocation = true;
+    // get location updates in every 10 seconds
+    this.intervalSubscriber = interval(5000).subscribe(() =>
+      this.getPosition()
+    );
+  }
+
+  public stopLocationTracking(): void {
+    this.isUpdatingLocation = false;
+    this.intervalSubscriber.unsubscribe();
+  }
 }
 
 export interface LOCATION {
-    lat: number;
-    lng: number;
+  lat: number;
+  lng: number;
 }
