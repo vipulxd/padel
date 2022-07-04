50679a22f0079cc6e6a36417305316a493210296
vipul dev <vipul.dev@thoughtwizards.com>
2022-07-04 14:15:00 +0300

    Notification feature added when app transitions . LocationService

--- agent-client/ios/App/App/AppDelegate.swift
+++ agent-client/ios/App/App/AppDelegate.swift
@@ -1,18 +1,35 @@
 import UIKit
 import Capacitor
+import UserNotifications
 import CoreLocation
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate , CLLocationManagerDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate , CLLocationManagerDelegate , UNUserNotificationCenterDelegate {
 
     
     var window: UIWindow?
     var locationManager : CLLocationManager!
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .sound]) { (granted, error) in
+            // Enable or disable features based on authorization.
+            if error != nil {
+                print("Request authorization failed!")
+            } else {
+                print("Request authorization succeeded!")
+                self.showAlert()
+            }
+        }
         // Override point for customization after application launch.
         return true
     }
+    func showAlert() {
+        let objAlert = UIAlertController(title: "Alert", message: "Request authorization succeeded", preferredStyle: UIAlertController.Style.alert)
 
+        objAlert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        //self.presentViewController(objAlert, animated: true, completion: nil)
+
+    }
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
@@ -24,11 +41,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate , CLLocationManagerDelegat
         locationManager?.delegate = self
         locationManager?.allowsBackgroundLocationUpdates = true
         locationManager?.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        // Create a local notification to update user about locations being updated even in the background
+        let content = UNMutableNotificationContent()
+        content.title = NSString.localizedUserNotificationString(forKey: "Padel", arguments: nil)
+        content.body = NSString.localizedUserNotificationString(forKey: "Padel is running.", arguments: nil)
+        content.sound = UNNotificationSound.default
+        content.categoryIdentifier = "notify-test"
+
+        let trigger = UNTimeIntervalNotificationTrigger.init(timeInterval: 1, repeats: false)
+        let request = UNNotificationRequest.init(identifier: "notify-test", content: content, trigger: trigger)
+
+        let center = UNUserNotificationCenter.current()
+        center.add(request)
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
     func stopLocationManagerFromSendingLocationUpdate(){
         locationManager?.stopUpdatingLocation()
+        UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
+        UNUserNotificationCenter.current().removeAllDeliveredNotifications()
     }
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
@@ -40,8 +71,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate , CLLocationManagerDelegat
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
+    
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
+    
 
     func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
         
