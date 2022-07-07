e6eca242b7bdf63bb35b300545eb0edc2ee689d5
vipuldev <vipul.xtr@gmail.com>
2022-07-07 23:07:14 +0530

    Map Module Testing.

--- admin-client/src/Components/userMap.js
+++ admin-client/src/Components/userMap.js
@@ -1,36 +1,47 @@
 import React, {useEffect, useState} from 'react'
-import {Map, Marker} from "pigeon-maps";
-import Cluster from 'pigeon-cluster'
+import { MapContainer,Marker,   TileLayer} from "react-leaflet";
+import 'leaflet/dist/leaflet.css';
+import marker from './../icons/locationMarker.svg'
+import L from 'leaflet';
+const newicon = new L.Icon({
+    iconUrl: marker,
+    iconSize: [30, 30]
+});
 
 export const UserMap = () => {
     const agentId = '6081abde-aa64-43a6-9601-e628ef38a9dc';
     const [data, setData] = useState([])
     const makeAPICall = async () => {
         try {
-            const response = await fetch(`http://localhost:2001/api/agent/location/${agentId}`, {mode: 'cors'});
+            const response = await fetch(`https://padel-config-api0server.herokuapp.com/api/agent/location/${agentId}`, {mode: 'cors'});
             const data = await response.json();
             setData(data.locations)
         } catch (e) {
-            console.log(e)
         }
     }
+
 
     useEffect(() => {
         makeAPICall()
     }, [])
     return (
-        data.length > 0 && <div>
-            <Map height={'100vh'}
-                 defaultCenter={[Number(data[data.length - 1].latitude), Number(data[data.length - 1].longitude)]}
-                 defaultZoom={18}>
-                {data.map((item, i ) => (
-                    <Marker key={i} width={30}
-                            anchor={[Number(item.latitude), Number(item.longitude)]}/>
-                ))}
-                <img src="./../../public/icons/locationMarker.svg" width={150} height={150} alt=""/>
-            </Map>
+        data.length > 0 && (
+            <MapContainer
+               center={{lat:data[data.length -1].latitude,lng:data[data.length -1].longitude}}
+                zoom={20}
+                style={{height: "100vh", width: "100%"}}
+                scrollWheelZoom={true}> <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+                {data &&  data.map((item,index)=>(
+                         <Marker key={index} position={[item.latitude,item.longitude]} icon={newicon} />
+                     ))
+               }
 
-        </div>
+           </MapContainer>
+        )
+
     )
 }
 
