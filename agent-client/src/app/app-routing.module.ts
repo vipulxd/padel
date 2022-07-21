a44da15cf6b9b8af4c333f35b9f84cac4ed85276
vipul dev <vipul.dev@thoughtwizards.com>
2022-07-21 15:38:45 +0300

    Android build in testing.

--- agent-client/src/app/app-routing.module.ts
+++ agent-client/src/app/app-routing.module.ts
@@ -1,28 +1,29 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [
-{
-  path: 'map',
-  loadChildren : ()=> import('./modules/usermap/usermap.module').then(m => m.UsermapModule)
-},
-{
-  path : 'profile',
-  loadChildren : ()=> import('./modules/userprofile/userprofile.module').then(m=> m.UserprofileModule)
-},
     {
-        path : 'logs',
-        loadChildren: ()=> import('./modules/userlogs/userlogs.module').then(m=> m.UserlogsModule)
+        path: 'map',
+        loadChildren: () => import('./modules/usermap/usermap.module').then(m => m.UsermapModule)
+    },
+    {
+        path: 'profile',
+        loadChildren: () => import('./modules/userprofile/userprofile.module').then(m => m.UserprofileModule)
+    },
+    {
+        path: 'logs',
+        loadChildren: () => import('./modules/userlogs/userlogs.module').then(m => m.UserlogsModule)
     }
-  ,  {
-    path : '',
-    redirectTo: 'map',
-    pathMatch : 'full'
-  }
+    , {
+        path: '',
+        redirectTo: 'map',
+        pathMatch: 'full'
+    }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule {
+}
